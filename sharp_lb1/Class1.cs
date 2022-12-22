61d2f669e90bd3569473beccb4d0c66dd523130b
Alex <Alex@DESKTOP-0HIIQ42>
2022-12-22 03:17:47 +0200

    Добавьте файлы проекта.

---
+++ sharp_lb1/Class1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp_lb1
+{
+    enum taryfs
+    {
+        Basic,
+        Advanced,
+        Unlium
+    }
+    enum services
+    {
+        UnliumMessenger = 40,
+        UnliumVideo = 10,
+        ExtraInternet = 73,
+        ExtraMinutes = 50
+    }
+
+    interface IAccount
+    {
+        public void Change_Taryf(taryfs new_taryf);
+        public double Make_Call(double amount_of_minites_in_call, long outgoing_number);
+        public void Connect_Service(services service_to_connect);
+        public void Disconnect_Service(int service_to_disconnect);
+        public void Replenish_Account(double replanish_amount);
+    }
+    class MobileAccount : IAccount
+    {
+        private taryfs current_taryf;
+        private long[] call_history;
+
+        private double one_minute_cost;
+        private double balance;
+        private int phone_number;
+        private services[] connected_services; //list service
+
+        public MobileAccount()
+        {
+            current_taryf = taryfs.Basic;
+            call_history = new long[0];
+
+            one_minute_cost = 1;
+            balance = 0;
+            phone_number = 0951234567;
+            connected_services = new services[0];
+        }
+        public MobileAccount(taryfs current_taryf, double one_minute_cost, double balance, int phone_number)
+        {
+            this.current_taryf = current_taryf;
+            call_history = new long[0];
+
+            this.one_minute_cost = one_minute_cost;
+            this.balance = balance;
+            this.phone_number = phone_number;
+            connected_services = new services[0];
+        }
+
+        public void Change_Taryf(taryfs new_taryf)
+        {
+            current_taryf = new_taryf;
+        }
+
+        public double Make_Call(double amount_of_minites_in_call, long outgoing_number)
+        {
+            double call_cost = amount_of_minites_in_call * one_minute_cost;
+            if (balance < call_cost)
+            {
+                throw new ArgumentException($"Недостатньо коштiв! Поповнiть рахунок на {call_cost} гривень.");
+            }
+            else
+            {
+                balance -= call_cost;
+                Array.Resize(ref call_history, call_history.Length + 1);
+                call_history[call_history.Length - 1] = outgoing_number;
+                return call_cost;
+            }
+        }
+
+        public void Connect_Service(services service_to_connect)
+        {
+            if (balance < (int)service_to_connect)
+            {
+                throw new ArgumentException($"Недостатньо коштiв! Поповнiть рахунок на {(int)service_to_connect} гривень.");
+            }
+            Array.Resize(ref connected_services, connected_services.Length + 1);
+            connected_services[connected_services.Length - 1] = service_to_connect;
+            balance -= (int)service_to_connect;
+        }
+        public void Disconnect_Service(int service_to_disconnect)
+        {
+            if (connected_services.Length > 0)
+            {
+                for (int i = service_to_disconnect; i < connected_services.Length - 1; i++)
+                {
+                    connected_services[i] = connected_services[i + 1];
+                }
+                Array.Resize(ref connected_services, connected_services.Length - 1);
+            }
+            else
+            {
+                throw new ArgumentException("Вiдсутнi пiдключенi послуги!");
+            }
+        }
+
+        public void Replenish_Account(double replanish_amount)
+        {
+            balance += replanish_amount;
+        }
+
+        public double Get_Balance
+        {
+            get { return balance; }
+        }
+        public long[] Get_History
+        {
+            get { return call_history; }
+        }
+        public services[] Get_Connected_Services
+        {
+            get { return connected_services; }
+        }
+        public taryfs Get_Taryf
+        {
+            get { return current_taryf; }
+        }
+    }
+}
---
+++ sharp_lb1/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace sharp_lb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //services[] serv = new services[2] { services.UnliumVideo, services.UnliumMessenger };
+            MobileAccount account = new MobileAccount(taryfs.Basic, 2, 100, 0957845654);
+            int user_choise;
+            do
+            {
+                Console.WriteLine("Оберiть дiю:\n" +
+                    "1. Змiнити тариф\n" +
+                    "2. Зробити дзвiнок\n" +
+                    "3. Пiдключити послугу\n" +
+                    "4. Вiдключити послугу\n" +
+                    "5. Поповнити баланс\n" +
+                    "6. Дiзнатись iсторiю викликiв\n" +
+                    "7. Перевiрити баланс\n" +
+                    "8. Дiзнатись поточний тариф\n" +
+                    "0. Закрити програму\n");
+                user_choise = int.Parse(Console.ReadLine());
+                try
+                {
+                    switch (user_choise)
+                    {
+                        case 1:
+                            Console.WriteLine("Доступнi тарифи:\n" +
+                                "1. Basic\n" +
+                                "2. Advanced\n" +
+                                "3. Unlium\n");
+                            int new_taryf_choise = int.Parse(Console.ReadLine());
+                            account.Change_Taryf((taryfs)new_taryf_choise - 1);
+                            Console.WriteLine("Тариф успiшно змiнено!\n");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Введiть номер телефону, на який " +
+                                "хочете здiйснити дзвiнок та кiлькiсть хвилин у розмовi.");
+
+                            Console.Write("Номер телефону: ");
+                            long new_outgoing_number = long.Parse(Console.ReadLine());
+                            Console.Write("К-сть хвилин: ");
+                            double mins_in_new_call = double.Parse(Console.ReadLine());
+                            Console.WriteLine($"Вартість дзвінка: {account.Make_Call(mins_in_new_call, new_outgoing_number)}");
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Оберiть послугу, яку хочете пiдключити. Введiть цифру вiд 1 до 4:");
+                            foreach (var item in Enum.GetValues(typeof(services)))
+                            {
+                                Console.WriteLine(item);
+                            }
+                            int service_to_connect = int.Parse(Console.ReadLine());
+
+                            switch (service_to_connect)
+                            {
+                                case 1:
+                                    account.Connect_Service(services.UnliumMessenger);
+                                    Console.WriteLine($"Послугу пiдключено! З вашого балансу знято {(int)services.UnliumMessenger} гривень.");
+                                    break;
+
+                                case 2:
+                                    account.Connect_Service(services.UnliumVideo);
+                                    Console.WriteLine($"Послугу пiдключено! З вашого балансу знято {(int)services.UnliumVideo} гривень.");
+                                    break;
+
+                                case 3:
+                                    account.Connect_Service(services.ExtraInternet);
+                                    Console.WriteLine($"Послугу пiдключено! З вашого балансу знято {(int)services.ExtraInternet} гривень.");
+                                    break;
+
+                                case 4:
+                                    account.Connect_Service(services.ExtraMinutes);
+                                    Console.WriteLine($"Послугу пiдключено! З вашого балансу знято {(int)services.ExtraMinutes} гривень.");
+                                    break;
+                            }
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Оберiть послугу, яку хочете вiдключити:");
+                            for (int i = 0; i < account.Get_Connected_Services.Length; i++)
+                            {
+                                Console.WriteLine($"{(i + 1)}. {account.Get_Connected_Services[i]}");
+                            }
+                            int service_to_disconnect = int.Parse(Console.ReadLine());
+                            account.Disconnect_Service(service_to_disconnect - 1);
+                            Console.WriteLine("Послуга успiшно вiдключена!\n");
+                            break;
+
+                        case 5:
+                            Console.Write("Введiть суму поповнення: ");
+                            double replanish_amount = double.Parse(Console.ReadLine());
+                            account.Replenish_Account(replanish_amount);
+                            Console.WriteLine("Баланс успiшно поповнено!");
+                            break;
+
+                        case 6:
+                            Console.WriteLine("Вашi останнi виклики:");
+                            for (int i = 0; i < account.Get_History.Length; i++)
+                            {
+                                Console.WriteLine($"{(i + 1)}: {account.Get_History[i]}");
+                            }
+                            break;
+
+                        case 7:
+                            Console.WriteLine($"Ваш поточний рахунок: {account.Get_Balance} гривень.");
+                            break;
+
+                        case 8:
+                            Console.WriteLine($"Ваш поточний тариф: {account.Get_Taryf}");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception: {ex.Message}");
+                }
+
+                //Console.Clear();
+            } while (user_choise > 0);
+        }
+    }
+}
